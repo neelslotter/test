d8da5433ffb6056a3f8f964ff3d4dfcf897a7225
neelslotter <neelslotter@gmail.com>
2019-03-15 20:40:35 +0200

    heelo again again ..

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        for (int x=0;x< 11;x++){
-            System.out.println("Hello World ! " + x);
+        for (int x=0;x< 10;x++){
+            System.out.println("Hello World ! "+ x);
+        }
+
+        for (int x=0;x< 10;x++) System.out.println("Hello World ! "+ x);
+
+        IntStream.range(0, 10).mapToObj(x -> "Hello World !").forEach(System.out::println);
+
+        IntStream.range(0, 10).mapToObj(x -> "Hello World !").forEachOrdered(System.out::println);
+
+        int x=0;
+        while (x< 10) {
+            System.out.println("Hello World !" + x);
+            x++;
+        }
+
+        int x1;
+        for (x1 = 0; x1< 10; x1++){
+            System.out.println("Hello World !");
         }
 
     }
