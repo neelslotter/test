2d775280e1853ccb71d44943ef1c7c708f458462
neelslotter@gmail.com <neelslotter@gmail.com>
2019-10-23 11:04:13 +0200

    change to method testerlgb
    Signed-off-by: neelslotter@gmail.com <neelslotter@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        doLoop(1, 1000);
+        //doLoop(1, 1000);
+        doBackward(1000, 1);
+    }
 
+    private static void doBackward(int aMin, int aMax) {
+
+
+        for (int y = 100; y != 10000; y++) {
+            System.out.println("Hello World ! " + y);
+        }
 
     }
 
@@ -30,12 +38,6 @@ public class Main {
         for (x1 = 0; x1 < aMax; x1++) {
             System.out.println("Hello World !" + x1);
         }
-
-        int y;
-        for (y = aMax; y != 5; y--) System.out.println("Hello World !" + y);
-
-        int k;
-        for (k = aMax; k != 5; k--) System.out.println("Hello World ! k " + k);
 
 
     }
