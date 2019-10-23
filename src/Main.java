cc33e897806b00b5dd7e082f34fd76b177974278
Neels Lotter <neelslotter@gmail.com>
2019-10-23 10:47:42 +0200

    change to method tester
    Signed-off-by: neelslotter@gmail.com <neelslotter@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int x=0;x< 10;x++){
-            System.out.println("Hello World ! "+ x);
+        doLoop(1, 1000);
+
+
+    }
+
+    private static void doLoop(int aMin, int aMax) {
+        for (int x = aMin; x < aMax; x++) {
+            System.out.println("Hello World ! " + x);
         }
 
-        for (int x=0;x< 10;x++) System.out.println("Hello World ! "+ x);
+        for (int x = aMin; x < 10; x++) System.out.println("Hello World ! " + x);
 
-        IntStream.range(0, 10).mapToObj(x -> "Hello World !" + x).forEach(System.out::println);
+        IntStream.range(aMin, aMax).mapToObj(x -> "Hello World !" + x).forEach(System.out::println);
 
-        IntStream.range(0, 10).mapToObj(x -> "Hello World !" + x).forEachOrdered(System.out::println);
+        IntStream.range(aMin, aMax).mapToObj(x -> "Hello World !" + x).forEachOrdered(System.out::println);
 
-        int x=0;
-        while (x< 10) {
+        int x = 0;
+        while (x < aMax) {
             System.out.println("Hello World !" + x);
             x++;
         }
 
         int x1;
-        for (x1 = 0; x1< 10; x1++){
+        for (x1 = 0; x1 < aMax; x1++) {
             System.out.println("Hello World !" + x1);
         }
-        
+
         int y;
-        for (y = 110; y != 5; y--) System.out.println("Hello World !" + y);
+        for (y = aMax; y != 5; y--) System.out.println("Hello World !" + y);
 
         int k;
-        for (k = 110; k != 5; k--) System.out.println("Hello World ! k " + k);
+        for (k = aMax; k != 5; k--) System.out.println("Hello World ! k " + k);
+
 
     }
 }
